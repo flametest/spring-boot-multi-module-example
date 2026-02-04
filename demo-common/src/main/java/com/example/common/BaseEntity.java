268af84d6db0dcc9305472dc6601767104430faa
flametest <flametest@gmail.com>
2026-02-05 00:29:10 +0800

    feat: module update.

---
+++ demo-common/src/main/java/com/example/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.common;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity implements Serializable {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createdAt;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updatedAt;
+    @TableField(fill = FieldFill.INSERT)
+    private String createdBy;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updatedBy;
+    @TableLogic
+    private LocalDateTime deletedAt;
+    private String deletedBy;
+}
---
+++ demo-user/demo-user-service/src/main/java/com/example/user/service/entity/User.java
@@ -0,0 +1,8 @@
+package com.example.user.service.entity;
+
+import com.example.common.BaseEntity;
+
+public class User extends BaseEntity {
+    private String firstName;
+    private String lastName;
+}
