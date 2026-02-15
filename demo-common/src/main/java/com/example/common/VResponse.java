1596ef7b7cadf5c18d1de4357510eb561a55736f
flametest <flametest@gmail.com>
2026-02-16 01:09:34 +0800

    feat: add response advice.

--- demo-common/src/main/java/com/example/common/VResponse.java
+++ demo-common/src/main/java/com/example/common/VResponse.java
@@ -5,8 +5,18 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
-public class VResponse {
+public class VResponse<T> {
     public final Integer code;
     public final String desc;
-    public final Object data;
+    public final T data;
+
+    public VResponse(T data) {
+        this.code = 0;
+        this.desc = "success";
+        this.data = data;
+    }
+
+    public static <T> VResponse<T> success(T data) {
+        return new VResponse<>(data);
+    }
 }
--- demo-server/src/main/java/com/example/server/config/ResponseAdvice.java
+++ demo-server/src/main/java/com/example/server/config/ResponseAdvice.java
@@ -1,4 +1,43 @@
 package com.example.server.config;
 
-public class ResponseAdvice {
+import com.example.common.DemoException;
+import com.example.common.ErrorDefEnum;
+import com.example.common.VResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true; // intercept all request
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof VResponse) {
+            return body;
+        }
+        return VResponse.success(body);
+    }
+
+    @ExceptionHandler(DemoException.class)
+    public VResponse<?> demoException(DemoException e) {
+        log.error("Demo Exception", e);
+        return new VResponse<>(e.getErrorDefEnum().getCode(), e.getErrorDefEnum().getDesc(), null);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public VResponse<?> exception(Exception e) {
+        log.error("Exception", e);
+        return new VResponse<>(500, e.getMessage(), null);
+    }
 }
